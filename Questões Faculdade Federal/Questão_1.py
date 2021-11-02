d0cce740efb9a205278a6f1b94a2c43396151acf
WagnerChicarelli <chicarelli.wagner@gmail.com>
2021-11-02 11:56:39 -0300

    Quarto programa - atualização

--- Questões Faculdade Federal/Questão_1.py
+++ Questões Faculdade Federal/Questão_1.py
@@ -1,8 +1,12 @@
 import os
+import time
 def questao():
     explicacao()
+    time.sleep(5)
     remover_palavras = palavras_a_serem_removidas()
+    time.sleep(5)
     leitura_do_aquivo()
+    time.sleep(5)
     remocao_das_palavras(remover_palavras)
 
 def remocao_das_palavras(remover_palavras):
@@ -18,20 +22,20 @@ def leitura_do_aquivo():
     for linha in musica:
         linha = linha.strip()
         print(linha)
-    print("*************************************************************************************************************")
+    print("*******************************************************************************************************************")
 
 def palavras_a_serem_removidas():
     remover_palavras = input("Escreva as palavras que serão removidas:\n")
     print(os.path.splitext(os.path.basename("musica.txt"))[0])
-    print("*************************************************************************************************************")
+    print("*******************************************************************************************************************")
     return remover_palavras
     
 def explicacao():
-    print("*************************************************************************************************************")
-    print("*************************************************Bem-Vindo!**************************************************")
-    print("**Este programa ira remover algumas palavras de um arquivo txt de acordo com a definição de outro arquivo..**")
-    print("*************************************************************************************************************")
-    print("*************************************************************************************************************")
+    print("*******************************************************************************************************************")
+    print("*************************************************Bem-Vindo!********************************************************")
+    print("**Este programa ira remover algumas palavras de um arquivo txt de acordo com a definição com o que foi inserido..**")
+    print("*******************************************************************************************************************")
+    print("*******************************************************************************************************************")
     
 if(__name__ == "__main__"):
     questao()
