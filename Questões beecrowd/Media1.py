cca25087a4ac0ecb766d81c2212bbda9be69a8c1
WagnerChicarelli <chicarelli.wagner@gmail.com>
2021-11-05 19:07:26 -0300

    Exercicio

---
+++ Questões beecrowd/Media1.py
@@ -0,0 +1,46 @@
+import time
+import os
+def problema1005():
+    limpar_tela()
+    bem_vindo()
+    tempo_pausa()
+    limpar_tela()
+    resposta = ProcessarTarefa()
+    tempo_pausa()
+    limpar_tela()
+    RetornoLogico(resposta)
+    tempo_pausa()
+    limpar_tela()
+
+def tempo_pausa():
+    time.sleep(5)
+
+def RetornoLogico(resposta):
+    resposta  = float(resposta) 
+    print("A media entre os dois numeros que você : {:.5f}".format(resposta))
+
+def ProcessarTarefa():
+    primeiro_numero = float(input("Digite o primeiro numero:\n"))
+    segundo_numero = float(input("Digite o segundo numero:\n"))
+    resposta = Contrato(primeiro_numero, segundo_numero)
+    return resposta
+
+def Contrato(primeiro_numero, segundo_numero):
+    primeiro_numero = float(primeiro_numero)
+    segundo_numero = float(segundo_numero)
+    primeiro_numero = primeiro_numero  * 3.5
+    segundo_numero = segundo_numero * 7.5
+    resposta = (primeiro_numero + segundo_numero) / (3.5+7.5)
+    return resposta
+
+
+def bem_vindo():
+    print("******************************************************************")
+    print("*** Este programa ira fazer uma conta para tirar media simples ***")
+    print("******************************************************************")
+
+def limpar_tela():
+    os.system('cls') or None
+
+if(__name__ == "__main__"):
+    problema1005()
